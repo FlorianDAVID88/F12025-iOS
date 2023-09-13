7742f39cccf067b697f1882980fb473c884412c0
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2023-09-13 10:29:19 +0200

    Add XCode basic project

---
+++ F12025-iOS/F12025_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  F12025_iOSApp.swift
+//  F12025-iOS
+//
+//  Created by user234243 on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct F12025_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
