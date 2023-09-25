9bf89079bf56c2fee8b790f6757f543dc6824635
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2023-09-25 11:24:46 +0200

    Adding of the rest of the views (2)

---
+++ F12025-iOS/View/ItemsView.swift
@@ -0,0 +1,100 @@
+//
+//  ItemView.swift
+//  F12025-iOS
+//
+//  Created by user234243 on 9/14/23.
+//
+
+import SwiftUI
+
+struct ItemGPView: View {
+    @State var gp: GrandPrix
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            VStack {
+                FlagView(pays: gp.circuit.pays, height: 35)
+                Text("\(gp.dates[0])\n\(gp.dates[1])")
+                    .font(.custom("Formula1", size: 12))
+                    .padding(5)
+                    .background(.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+            }
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(gp.nom)
+                    .bold()
+                    .font(.custom("Formula1-Display-Bold", size: 20))
+                
+                Text(gp.circuit.nom)
+                    .font(.custom("Formula1", size: 14))
+                
+            }
+            .multilineTextAlignment(.leading)
+        }
+    }
+}
+
+struct ItemPilotView: View {
+    @State var pilot: Pilote
+    
+    var body: some View {
+        let pilotName = "\(pilot.prenom) \(pilot.nom)"
+        HStack(spacing: 20) {
+            Image(pilotName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 75)
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading) {
+                Text(pilotName)
+                    .font(.custom("Formula1-Display-BOLD", size: 20))
+                    .foregroundColor(.black)
+
+                HStack(spacing: 10) {
+                    FlagView(pays: pilot.nationalite, height: 20)
+                    Image(pilot.team.nom_ecurie)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 20)
+                }
+            }
+        }
+    }
+}
+
+struct ItemTeamView: View {
+    @State var team: Ecurie
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(team.nom_ecurie)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading) {
+                Text(team.nom_ecurie)
+                    .font(.custom("Formula1-Display-BOLD", size: 20))
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.black)
+                
+                FlagView(pays: team.nationalite, height: 20)
+            }
+        }
+        
+    }
+}
+
+struct ItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 15) {
+            ItemGPView(gp: GrandPrix.allCases[0])
+            ItemPilotView(pilot: Pilote.allCases[15])
+            ItemTeamView(team: Ecurie.allCases[4])
+        }
+    }
+}
---
+++ F12025-iOS/View/TestView.swift
@@ -0,0 +1,22 @@
+//
+//  TestView.swift
+//  F12025-iOS
+//
+//  Created by user234243 on 9/15/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
