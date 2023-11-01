965374567cac72b4c5c11b62ef9978f02a416da7
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2023-11-02 00:31:31 +0100

    Create UserApp.swift
    Has the same structure as FIRUser (Firebase is used for the authentification)

---
+++ F12025-iOS/Model/UserApp.swift
@@ -0,0 +1,36 @@
+//
+//  UserApp.swift
+//  F1 2025
+//
+//  Created by user234243 on 10/28/23.
+//
+
+import Foundation
+import Firebase
+import GoogleSignIn
+
+/**
+ * Class based on the FIRUser structure
+ * Purpose : to facilitate its encoding so that it can be stored in UserDefaults
+ */
+struct UserApp: Codable {
+    var id: String
+    var name: String
+    var email: String
+    var phoneNumber: String
+    var photoURL: URL?
+    var isAnonymous: Bool
+    var isEmailVerified: Bool
+    var connexion: String       // The connexion method (Apple, Google or Email)
+    
+    init(from user: User, connexion: String) {
+        self.id = user.uid
+        self.name = user.displayName ?? "User"
+        self.email = user.email ?? ""
+        self.phoneNumber = user.phoneNumber ?? ""
+        self.photoURL = user.photoURL
+        self.isAnonymous = user.isAnonymous
+        self.isEmailVerified = user.isEmailVerified
+        self.connexion = connexion
+    }
+}
